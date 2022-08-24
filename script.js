964c4e1fd139436714b4d90d2bcc9434543285e9
Victor Feliciano <90053045+jabufeliciano@users.noreply.github.com>
2022-08-24 18:32:17 -0300

    primeiro site

---
+++ script.js
@@ -0,0 +1,104 @@
+function john(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/John_Lennon'
+        
+    }, 0)
+}
+
+function kurt(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Kurt_Cobain'
+        
+    }, 0)
+}
+
+function freddy(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Freddie_Mercury'
+        
+    }, 0)
+}
+
+function Mick(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Mick_Jagger'
+        
+    }, 0)
+}
+
+function Pedro(){
+    setTimeout(()=>{
+        window.location.href = 'https://linktr.ee/rebornbot'
+        
+    }, 0)
+}
+
+function Victor(){
+    setTimeout(()=>{
+        window.location.href = 'https://www.instagram.com/victor_feliciano777/'
+        
+    }, 0)
+}
+
+function Junior(){
+    setTimeout(()=>{
+        window.location.href = 'https://www.instagram.com/jr.nds/'
+        
+    }, 0)
+}
+
+function beatles(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/The_Beatles'
+        
+    }, 0)
+}
+
+function rolling(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/The_Rolling_Stones'
+        
+    }, 0)
+}
+
+function queen(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Queen'
+        
+    }, 0)
+}
+
+function redhot(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Red_Hot_Chili_Peppers'
+        
+    }, 0)
+}
+
+function nirvana(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Nirvana_(banda)'
+        
+    }, 0)
+}
+
+function ramones(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Ramones'
+        
+    }, 0)
+}
+function ramones(){
+    setTimeout(()=>{
+        window.location.href = 'https://pt.wikipedia.org/wiki/Ramones'
+        
+    }, 0)
+}
+
+function conttt(){
+    setTimeout(()=>{
+        alert('Obrigado pela mensagem, assim que possivel iremos retornar-la!!!!')
+        window.location.href = '#banner'
+        
+    }, 0)
+}
